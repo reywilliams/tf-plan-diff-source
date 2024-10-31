10da699e50a2358b72cc53b4529b0ea6b99e6118
Rey Williams <reynaldomwilliams@gmail.com>
2024-10-31 01:03:36 -0700

    add debug during config

--- src/config/config.go
+++ src/config/config.go
@@ -26,19 +26,29 @@ func ConfigFromAction(action *githubactions.Action) (*Config, error) {
 	if filePath == "" {
 		return nil, fmt.Errorf("%s input was empty", FILE_PATH_INPUT_KEY)
 	}
+	action.Debugf("usings file path: %s", filePath)
 
 	appName := action.GetInput(APP_NAME_INPUT_KEY)
+	action.Debugf("usings app name: %s", appName)
 
-	includeReadActions := false
+	var includeReadActions bool
 	includeReadInput := action.GetInput(INCLUDE_READ_INPUT_KEY)
 	if includeReadInput != "" {
 		includeReadActions = true
+		action.Debugf("read actions enabled and will be included in plan diff")
+	} else {
+		includeReadActions = false
+		action.Debugf("read actions disabled and will be not included in plan diff")
 	}
 
-	includeNoOpActions := false
+	var includeNoOpActions bool
 	includeNoOpInput := action.GetInput(INCLUDE_NOOP_INPUT_KEY)
 	if includeNoOpInput != "" {
-		includeNoOpActions = true
+		includeReadActions = true
+		action.Debugf("no-op actions enabled and will be included in plan diff")
+	} else {
+		includeNoOpActions = false
+		action.Debugf("no-op actions disabled and will be not included in plan diff")
 	}
 
 	cfg := Config{FilePath: filePath,
